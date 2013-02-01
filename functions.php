71c5d7574e6884d2e88ef56d94b0ec01b4cf027d
Remkus de Vries <remkus@forsite.nu>
2013-02-01 18:38:38 +0100

    Prefix all the things
    Prefix all the functions and fixing a few more small things.
    Readability being one. Still lacking proper DocBlocks though.

--- functions.php
+++ functions.php
@@ -4,12 +4,14 @@ require_once( get_template_directory() . '/lib/init.php' );
 
 // Child theme (do not remove)
 define( 'CHILD_THEME_NAME', 'BreakPoint Theme' );
-define( 'CHILD_THEME_URL', 'http://www.themecraft.com.com/demo/breakpoint' );
+define( 'CHILD_THEME_URL', 'http://www.themecraft.com/demo/breakpoint' );
 
 // Add Viewport meta tag for mobile browsers
-add_action( 'genesis_meta', 'sample_viewport_meta_tag' );
-function sample_viewport_meta_tag() {
+add_action( 'genesis_meta', 'breakpoint_viewport_meta_tag' );
+function breakpoint_viewport_meta_tag() {
+	
 	echo '<meta name="viewport" content="width=device-width, initial-scale=1.0"/>';
+
 }
 
 // Add support for custom background
@@ -24,24 +26,30 @@ add_theme_support( 'genesis-custom-header', array(
 // Custom image sizes
 add_image_size( 'Content Image', 760, 430, TRUE );
 
-// Add Header Wrap
-add_action('genesis_before_header',  'header_wrapper_div_open');
-function header_wrapper_div_open() {
+// Add Header Wrap 
+add_action( 'genesis_before_header',  'breakpoint_header_wrapper_div_open' );
+function breakpoint_header_wrapper_div_open() {
+	
 	echo '<div id="headerwrapper">';
+
 }
 
-add_action('genesis_after_header',  'header_wrapper_div_close', 1);
-function header_wrapper_div_close() {
+add_action( 'genesis_after_header',  'breakpoint_header_wrapper_div_close', 1 );
+function breakpoint_header_wrapper_div_close() {
+	
 	echo '</div>';
 }
 
 // Add the after post section
-add_action( 'genesis_after_post_content', 'after_post_area' );
-function after_post_area() {
+add_action( 'genesis_after_post_content', 'breakpoint_after_post_area' );
+function breakpoint_after_post_area() {
+    
     if ( is_singular( 'post' ) )
+    
     genesis_widget_area( 'after-post', array(
         'before' => '<div class="after-post-area widget-area">',
-    ) );
+    	) 
+	);
 }
 
 // Add support for 3-column footer widgets
@@ -49,7 +57,8 @@ add_theme_support( 'genesis-footer-widgets', 3 );
 
 // Register after post widget
 genesis_register_sidebar( array(
-	'id'		=> 'after-post',
-	'name'		=> __( 'After Post', 'breakpoint' ),
+	'id'			=> 'after-post',
+	'name'			=> __( 'After Post', 'breakpoint' ),
 	'description'	=> __( 'This is the after post area.', 'breakpoint' ),
-) );
+	) 
+);
