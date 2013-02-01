679684cd85964d811fec07c6bbf6797a228e19e1
Brad Potter <brad@aisak.com>
2013-01-31 23:59:48 -0900

    Initial Commit

---
+++ functions.php
@@ -0,0 +1,55 @@
+<?php
+// Start the engine
+require_once( get_template_directory() . '/lib/init.php' );
+
+// Child theme (do not remove)
+define( 'CHILD_THEME_NAME', 'BreakPoint Theme' );
+define( 'CHILD_THEME_URL', 'http://www.themecraft.com.com/demo/breakpoint' );
+
+// Add Viewport meta tag for mobile browsers
+add_action( 'genesis_meta', 'sample_viewport_meta_tag' );
+function sample_viewport_meta_tag() {
+	echo '<meta name="viewport" content="width=device-width, initial-scale=1.0"/>';
+}
+
+// Add support for custom background
+add_theme_support( 'custom-background' );
+
+// Add support for custom header
+add_theme_support( 'genesis-custom-header', array(
+	'width' => 1100,
+	'height' => 120
+) );
+
+// Custom image sizes
+add_image_size( 'Content Image', 760, 430, TRUE );
+
+// Add Header Wrap
+add_action('genesis_before_header',  'header_wrapper_div_open');
+function header_wrapper_div_open() {
+	echo '<div id="headerwrapper">';
+}
+
+add_action('genesis_after_header',  'header_wrapper_div_close', 1);
+function header_wrapper_div_close() {
+	echo '</div>';
+}
+
+// Add the after post section
+add_action( 'genesis_after_post_content', 'after_post_area' );
+function after_post_area() {
+    if ( is_singular( 'post' ) )
+    genesis_widget_area( 'after-post', array(
+        'before' => '<div class="after-post-area widget-area">',
+    ) );
+}
+
+// Add support for 3-column footer widgets
+add_theme_support( 'genesis-footer-widgets', 3 );
+
+// Register after post widget
+genesis_register_sidebar( array(
+	'id'		=> 'after-post',
+	'name'		=> __( 'After Post', 'breakpoint' ),
+	'description'	=> __( 'This is the after post area.', 'breakpoint' ),
+) );
---
+++ page_landing.php
@@ -0,0 +1,26 @@
+<?php
+/*
+Template Name: Landing
+*/
+
+// Add custom body class to the head
+add_filter( 'body_class', 'breakpoint_add_body_class' );
+function breakpoint_add_body_class( $classes ) {
+   $classes[] = 'breakpoint-landing';
+   return $classes;
+}
+
+// Remove the header right widget area
+unregister_sidebar( 'header-right' );
+
+// Remove header, navigation, breadcrumbs, footer widgets, footer 
+add_filter( 'genesis_pre_get_option_site_layout', '__genesis_return_full_width_content' );
+remove_action( 'genesis_after_header', 'genesis_do_nav' );
+remove_action( 'genesis_after_header', 'genesis_do_subnav' );
+remove_action( 'genesis_before_loop', 'genesis_do_breadcrumbs' );
+remove_action( 'genesis_before_footer', 'genesis_footer_widget_areas' );
+remove_action( 'genesis_footer', 'genesis_footer_markup_open', 5 );
+remove_action( 'genesis_footer', 'genesis_do_footer', 10 );
+remove_action( 'genesis_footer', 'genesis_footer_markup_close', 15 );
+
+genesis();
